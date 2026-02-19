319822de2b00b245a5723eda3af0a0b80e90b4dd
Giantrafa <giantrafa@gmail.com>
2026-02-19 20:26:45 -0300

    organisa

--- Qmain.java
+++ Qmain.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class Qmain {
     public static void main(String[] args) {
-        System.out.println("Digite o lado do quadrado: ");
         Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite o lado do quadrado: ");
+        
         int lado = sc.nextInt();
         Quadrado q1 = new Quadrado(lado);
+        
         System.out.println(q1.lado);
         System.out.println(q1.area());
         System.out.println(q1.comprimento());
+        
         q1.desenha(lado);
 
     }
